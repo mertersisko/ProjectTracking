f97563a1edfc94df30b71c561d0c5c3ec9cee3c4
Mert <mertersisko@gmail.com>
2023-07-14 11:22:41 +0300

    Proje sayfası

--- ProjectTracking.Presentation/Controllers/ProjectController.cs
+++ ProjectTracking.Presentation/Controllers/ProjectController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjectTracking.Bussiness.Repositories.Classes.ProjectServices.Abstract;
+using ProjectTracking.Bussiness.Repositories.Classes.RequisitionServices.Abstract;
 using ProjectTracking.DataAccess.Context;
 using ProjectTracking.DataAccess.Entites.Classes.DbClasses.ProjectClasses;
 using ProjectTracking.DataAccess.Entites.Classes.ViewModel;
@@ -8,15 +10,19 @@ namespace ProjectTracking.Presentation.Controllers;
 
 public class ProjectController : Controller
 {
+    private readonly ProjectTrackingDataContext _dataContext;
     private readonly IProjectService _projectService;
+    private readonly IRequisitionService _requisitionService;
 
-    public ProjectController(IProjectService projectService)
+    public ProjectController(IProjectService projectService, ProjectTrackingDataContext dataContext)
     {
+        _dataContext = dataContext;
         _projectService = projectService;
     }
     public async Task<IActionResult> Index()
     {
         var projectList = await _projectService.ActiveProject();
+        //ViewBag.RequisitionList = new SelectList(_dataContext.Requisitions.ToList(), "Id", "RequisitionsName");
         return View(projectList.DataList);
     }
     public IActionResult Add()
